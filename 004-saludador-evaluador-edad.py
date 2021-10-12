97ceb5610c7bb5d090e3fe6e3ec1c97609ff74ef
juanky35 <jkarl219@gmail.com>
2021-10-12 20:09:16 +0200

    Create 004-saludador-evaluador-edad.py

---
+++ 004-saludador-evaluador-edad.py
@@ -0,0 +1,12 @@
+nombre=input("como te llamas:")
+edad=input("Qué edad tienes:")
+edad=int(edad)
+if edad>=18:
+    print("Buenos días:",nombre, "Eres mayor de edad, bienvenido/a crack")
+else:
+    print("Error mortal de la muerte muerta, comando de auto destruccion activado...")
+    print("Cuenta atras iniciada...")
+    print("59...")
+    print("58...")
+    print(nombre, ":Sin autorización de acceso, brecha de seguridad")
+    print("Eres menor de edad")
