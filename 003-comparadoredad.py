a4075d645f57d99273b619e15bc05060570e3082
juanky35 <jkarl219@gmail.com>
2021-10-12 20:09:14 +0200

    Create 003-comparadoredad.py

---
+++ 003-comparadoredad.py
@@ -0,0 +1,8 @@
+edad=input("Dime tu edad:")
+edad=int(edad)
+if edad>=18:
+    print("Eres mayor de edad")
+    print("macho alfa, espalda plateada, pecho peludo, crack, monstruo, mastodonte, mamut")
+else:
+    print("Eres menor de edad")
+    print("aún un yogurin")
