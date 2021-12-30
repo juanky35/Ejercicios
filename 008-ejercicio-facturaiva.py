d35b70f69b06718ec5e75ee56fb43e17e9770fd9
juanky35 <jkarl219@gmail.com>
2021-12-30 18:34:14 +0100

    Update 008-ejercicio-facturaiva.py

--- 008-ejercicio-facturaiva.py
+++ 008-ejercicio-facturaiva.py
@@ -1,9 +1,9 @@
 importe1=input("Escribe el primer importe: ")
-importe1=int(importe1)
+importe1=float(importe1)
 importe2=input("Escribe el segundo importe: ")
-importe2=int(importe2)
+importe2=float(importe2)
 iva=input("Introduce el IVA aplicado: ")
-iva=int(iva)
+iva=float(iva)
 resultado=(importe1+importe2)*(1+iva/100)
 miNumeroRedondeado=round(resultado,2)
 if  importe1<0:
